af5f2bb8dfe1b08904fa39c3f0d6beaeb44b153d
Vesso927 <54169475+Vesso927@users.noreply.github.com>
2019-08-19 11:08:14 +0800

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+
+urls = (
+    '/wx', 'Handle',
+)
+
+
+class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "1234"
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print
+            "handle/GET func: hashcode, signature: ", hashcode, signature
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception, Argument:
+            return Argument
+
+
+if __name__ == '__main__':
+    app = web.application(urls, port=80, globals())
+    app.run()
